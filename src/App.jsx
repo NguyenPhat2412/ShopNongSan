84c101b6e3fc5832183cdc4867ddee71cb54db88
Phat Nguyen <masterrio2412@gmail.com>
2025-07-19 21:19:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
